dfd7af8a1020b3b252b124b1604fc308289d4279
Ultraempoleon <44034263+Ultraempoleon@users.noreply.github.com>
2021-11-16 21:48:58 -0800

    No real changes
    Added monster class

--- Combat Tracker/encounter_roller.py
+++ Combat Tracker/encounter_roller.py
@@ -1,9 +1,18 @@
 import random
 
-class attack:
+class monster:
+    def __init__(self, name, armor_class, hitdice_amount, hit_dice, hp_mod):
+        self.name = name
+        self.armor_class = armor_class
+        self.hitdice_amount = hitdice_amount
+        self.hit_dice = hit_dice
+        self.hp_mod = hp_mod
+
+
+class attack():
     def __init__(self, to_hit, dmg_dice_amount, dmg_dice, dmg_mod, \
                 dmg_type, dmg_dice2_amount=0, dmg_dice2=0, dmg_type2="",\
-                dmg_mod2=0):
+                dmg_mod2=0, recharge1 = False, recharge2 = False):
 
         self.to_hit = to_hit
         self.dmg_dice_amount = dmg_dice_amount
@@ -16,20 +25,27 @@ class attack:
         self.dmg_type2 = dmg_type2
         self.dmg_mod2 = dmg_mod2
 
+    #Calcs roll and damage dealt
     def total_damage(self):
         roll_to_hit = random.randrange(1, 20) + self.to_hit
         rolleddamage = random.randrange(1, self.dmg_dice) + self.dmg_mod
+
+
         if self.dmg_dice2 > 0:
-            rolleddamage += random.randrange(1, self.dmg_dice2) + self.dmg_mod2
-        
-        return roll_to_hit, rolleddamage
+            rolleddamage2 = random.randrange(1, self.dmg_dice2) + self.dmg_mod2
+            total_rolleddamage = rolleddamage2 + rolleddamage
+            print("{} : {} damage | {} {} and {} {}".format(roll_to_hit, 
+                total_rolleddamage, rolleddamage, self.dmg_type, 
+                rolleddamage2, self.dmg_type2))    
+        else:
+            print("{} : {} {}".format(roll_to_hit, rolleddamage, self.dmg_type))
 
 
 def main():
-    newattack = attack(5, 1, 8, 3, 'piercing')
-    roll_to_hit, rolled_damage = attack.total_damage(newattack)
-    print("{} : {} {}".format(roll_to_hit, rolled_damage, newattack.dmg_type))
-
+    hellhound = monster('Hell Hound', 15, 7, 8, 14)
+    newattack = attack(5, 1, 8, 3, 'piercing', 2, 6, 'fire')
+    attack.total_damage(newattack)
+    
 
 if __name__=="__main__":
     main()
